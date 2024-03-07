fb8803b32e749c12c66b5f6e879021c2dafb2901
Tejas Panchal <84320606+Tejas-Panchal@users.noreply.github.com>
2024-03-07 23:31:49 +0530

    not yet

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 let isLoggedIn = false;
 
 function handleLogin(event) {
-    event.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+  event.preventDefault();
 
-    if (username === "admin" && password === "password") {
-        isLoggedIn = true;
-        window.location.href = "index.html";
-    } else {
-        alert("Invalid username or password. Please try again.");
-    }
-}
-if (!isLoggedIn && window.location.pathname !== "/login.html") {
-    window.location.href = "/login.html";
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "password") {
+    isLoggedIn = true;
+    redirectToIndex();
+  } else {
+    alert("Invalid username or password. Please try again.");
   }
+}
 
-document.getElementById("loginForm").addEventListener("submit", handleLogin);
+function redirectToLogin() {
+  document.getElementById("indexPage").classList.add("hidden");
+  document.getElementById("restrictedPage").classList.remove("hidden");
+  document.getElementById("loginPage").classList.remove("hidden");
+}
+
+function redirectToIndex() {
+  window.location.href = "index.html";
+}
+
+if (window.location.pathname !== "index.html" && !isLoggedIn) {
+  redirectToLogin();
+}
