33ff370029406f62189dbcf25ffec5dd45fe1f9e
Tejas Panchal <tejaspanchal625@gmail.com>
2024-08-15 13:18:13 +0530

    new design added

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
+$(".hamburger").click(function(){
+   $(".wrapper").toggleClass("collapse");
+});
+
+$(".sidebar a").click(function(e) {
+  e.preventDefault();
+  var pageId = $(this).data("page");
+  $(".page").hide();
+  $("#" + pageId).show();
+  $(".sidebar a").removeClass("active");
+  $(this).addClass("active");
+});
+
+// Show the first page by default
+$("#notes").show();
+
 document.onkeydown = (e) => {
   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
   if (
